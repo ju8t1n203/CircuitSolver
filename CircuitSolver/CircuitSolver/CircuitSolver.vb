3e279a357cd1fe154a9bc10b9bd9be6f0b7d9f2e
Ju8t1n203 <Juxin203@gmail.com>
2025-02-14 09:34:01 -0700

    Add project files.

---
+++ CircuitSolver/CircuitSolver/CircuitSolver.vb
@@ -0,0 +1,14 @@
+﻿'Justin Bell
+'RCET3371
+'S25
+'link
+
+Option Compare Text
+Option Explicit On
+Option Strict On
+
+Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
